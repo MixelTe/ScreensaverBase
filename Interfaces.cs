9473ff961b7b89551a650c7bfa0ca8b72d56981f
Mike <63655075+MixelTe@users.noreply.github.com>
2020-09-16 20:53:05 +0300

    init

---
+++ Interfaces.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace ScreensaverBase
+{
+	interface IDrawingBuffer : IDisposable
+	{
+		void Draw(IPainter painter);
+		void RenderTo(Graphics target);
+	}
+	internal interface IPainter : IDisposable
+	{
+		void Draw(Graphics g);
+	}
+
+	internal interface IController : IDisposable
+	{
+		void Update();
+	}
+}
---
+++ ScreensaverSettings.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace ScreensaverBase
+{
+	class ScreensaverSettings
+	{
+		private static readonly string _KeyName = @"HKEY_CURRENT_USER\Software\MixelTe\ScreenSaver";
+		public static int D_Speed = 1;
+
+		public int Speed = D_Speed;
+
+
+		public void Save()
+		{
+			Registry.SetValue(_KeyName, "Speed", Speed);
+		}
+		public void Load()
+		{
+			var speed = Registry.GetValue(_KeyName, "Speed", Speed);
+
+			if (speed != null) Speed = (int)speed;
+		}
+
+		public override string ToString()
+		{
+			return "Speed: " + Speed;
+		}
+	}
+}
